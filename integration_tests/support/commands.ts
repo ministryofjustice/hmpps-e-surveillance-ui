404031e4ed5969d4adc6b99cd5034384f2e05798
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-09-26 11:35:27 +0100

    fix redirection

--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -1,4 +1,3 @@
 Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
-  cy.request('/notifications')
   return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
 })
