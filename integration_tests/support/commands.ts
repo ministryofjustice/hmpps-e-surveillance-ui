ca7646027b92a7bab14bfda31daf198ff0244667
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-09-26 11:43:24 +0100

    fix redirection

--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -1,4 +1,12 @@
-Cypress.Commands.add('signIn', (options = { failOnStatusCode: false }) => {
-  cy.request('/')
-  return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
+// Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
+//   cy.request('/')
+//   return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
+// })
+cy.request({
+  url: '/',
+  followRedirect: false, // Don’t follow to see where it goes
+  failOnStatusCode: false, // Prevent 302 from throwing
+}).then(response => {
+  expect(response.status).to.eq(302)
+  expect(response.redirectedToUrl).to.include('/notifications')
 })
