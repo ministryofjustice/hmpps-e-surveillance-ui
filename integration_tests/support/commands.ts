f943cdeb6d8c14a2747176373c3b6f6503f07d61
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-09-26 12:11:10 +0100

    review comments

--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -1,18 +1,12 @@
-// Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
-//   cy.request('/')
-//   return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
-// })
 Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
-  return cy
-    .request({
-      url: '/',
-      followRedirect: false,
-      failOnStatusCode: false,
-    })
-    .then(response => {
-      expect(response.status).to.eq(302)
-      expect(response.redirectedToUrl).to.include('/notifications')
-
-      return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
-    })
+  cy.signIn()
+  cy.request('/')
+  return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
 })
+
+// Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
+//   return cy.task('getSignInUrl').then((url: string) => {
+//     cy.log(`Signing in via URL: ${url}`)
+//     return cy.visit(url, options)
+//   })
+// })
